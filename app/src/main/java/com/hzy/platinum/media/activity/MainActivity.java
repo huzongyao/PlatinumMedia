da282d8fa4aed4eed486dcdbde95fc7d1109d43c
huzongyao <hzy3774@gmail.com>
2018-06-10 00:25:42 +0800

    abi armeabi is no longer support

--- app/src/main/java/com/hzy/platinum/media/activity/MainActivity.java
+++ app/src/main/java/com/hzy/platinum/media/activity/MainActivity.java
@@ -1,18 +1,38 @@
 package com.hzy.platinum.media.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 import com.hzy.platinum.media.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button_simple).setOnClickListener(v ->
-                startActivity(new Intent(this, SimpleActivity.class)));
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+        mFab.setOnClickListener(v -> {
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
